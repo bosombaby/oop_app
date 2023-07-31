2dd52d0809b6a713a6cc54644e89dee943e0a369
bosom <1245924849@qq.com>
2023-07-31 15:57:15 +0800

    first commit

--- 04 摄像头/main.py
+++ 04 摄像头/main.py
@@ -1,10 +1,18 @@
 from kivy.app import App
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 
 
-class TestApp(App):
+class MyApp(App):
     def build(self):
-        return Button(text='iPaoMi')
+        label = Label(text='Hello Kivy', font_size=50)
+        button = Button(text='Click me!', background_color=(0, 0, 1, 1))
+        button.bind(on_press=self.on_button_press)
+        return label, button
+
+    def on_button_press(self, instance):
+        print('Button pressed!')
 
 
-TestApp().run()
+if __name__ == '__main__':
+    MyApp().run()
